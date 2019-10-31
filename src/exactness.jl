85a883e5979155d3ce4080ab124b463365d66617
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2019-10-31 18:11:13 -0300

    UPD removed INT support on src/exactness.jl

--- src/exactness.jl
+++ src/exactness.jl
@@ -4,7 +4,6 @@
 
 # Imports
 using Measurements: Measurement
-using Intervals: Interval
 using Unitful: Quantity
 
 
@@ -16,36 +15,12 @@ using Unitful: Quantity
 FLO = Union{Float16,Float32,Float64,BigFloat}
 
 # Exact types
-ETY{𝘁} = Union{Quantity{𝘁}} where 𝘁<:FLO
-
-## ```julia-repl
-## julia> typeof(measurement(0.1u"m", 0.01u"m"))
-## Quantity{Measurement{Float64},𝐋,Unitful.FreeUnits{(m,),𝐋,nothing}}
-## 
-## julia> typeof(Interval(0.1u"m", 0.01u"m"))
-## Interval{Quantity{Float64,𝐋,Unitful.FreeUnits{(m,),𝐋,nothing}}}
-## ```
+ETY{𝘁} = Quantity{𝘁} where 𝘁<:FLO
 
 # Measurement types
-MTY{𝘁} = Union{Quantity{Measurement{𝘁}}} where 𝘁<:FLO
+MTY{𝘁} = Quantity{Measurement{𝘁}} where 𝘁<:FLO
 
-# Interval types
-ITY{𝘁} = Union{Interval{Quantity{𝘁}}} where 𝘁<:FLO
-
-# Thermodynamic quantity
-QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁},ITY{𝘁}} where 𝘁<:FLO
-
-## ```julia-repl
-## julia> 0.1u"m" isa QTY{Float64}
-## true
-## 
-## julia> measurement(0.1f0, 0.01f0)u"m" isa QTY{Float32}
-## true
-## 
-## julia> Interval(0.1f0u"m", 0.2f0u"m") isa QTY{Float32}
-## false
-## ```
-
-# TODO: ditch Intervals, since it doesn't play well with others?
+# θ Quantity types
+QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:FLO
 
 
