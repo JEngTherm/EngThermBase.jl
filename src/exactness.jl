c773ddfd750bc1957c91f2a88dac8e7021639924
Naaktgeboren, C <NaaktgeborenC.PhD@gmail.com>
2019-11-06 15:27:54 -0300

    UPD completness of aux types on src/exactness.jl

--- src/exactness.jl
+++ src/exactness.jl
@@ -19,6 +19,13 @@ QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:𝖥
 #                                        Auxiliar Types                                        #
 #----------------------------------------------------------------------------------------------#
 
+# The 4 type quadrants are:
+# -------------------------
+#   bareF   QTY{𝘁}  | float-based
+#   bareR   unitR   | non-float reals
+#   -----------------
+#   plain   united
+
 # \sansR<TAB>: plain Julia Reals (since Unitful.Quantity <: Number)
 𝖱 = Union{AbstractIrrational,Integer,Rational}
 
@@ -27,5 +34,8 @@ bareF = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖥
 
 # Bare, unitless reals
 bareR = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖱
+
+# United reals
+unitR = Union{Quantity{𝘁}, Quantity{Measurement{𝘁}}} where 𝘁<:𝖱
 
 
