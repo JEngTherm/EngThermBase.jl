5430399346f4dd8287b4a5476b7f97d681663d93
Naaktgeboren, C <NaaktgeborenC.PhD@gmail.com>
2019-11-06 15:05:21 -0300

    UPD aux types on src/exactness.jl

--- src/exactness.jl
+++ src/exactness.jl
@@ -5,9 +5,6 @@
 # \sansF<TAB>: plain Julia Floats (since Measurements.Measurement <: AbstractFloat)
 𝖥 = Union{Float16,Float32,Float64,BigFloat}
 
-# \sansR<TAB>: plain Julia Reals (since Unitful.Quantity <: Number)
-𝖱 = Union{AbstractIrrational,Integer,Rational}
-
 # Exact types: all type params are \bsans#<TAB>
 ETY{𝘁} = Quantity{𝘁} where 𝘁<:𝖥
 
@@ -16,5 +13,19 @@ MTY{𝘁} = Quantity{Measurement{𝘁}} where 𝘁<:𝖥
 
 # θ Quantity types
 QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:𝖥
+
+
+#----------------------------------------------------------------------------------------------#
+#                                        Auxiliar Types                                        #
+#----------------------------------------------------------------------------------------------#
+
+# \sansR<TAB>: plain Julia Reals (since Unitful.Quantity <: Number)
+𝖱 = Union{AbstractIrrational,Integer,Rational}
+
+# Bare, unitless floats
+bareF = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖥
+
+# Bare, unitless reals
+bareR = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖱
 
 
