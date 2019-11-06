3892d96dc6a7e108805c7dba09306c7b67566e7d
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2019-11-05 23:21:05 -0300

    UPD propagated new type name on src/settings.jl

--- src/settings.jl
+++ src/settings.jl
@@ -2,16 +2,11 @@
 #                                       Package Defaults                                       #
 #----------------------------------------------------------------------------------------------#
 
-# This file is included right after abstract type definitions, and before anything else,
-# therefore, only abstract types must initialize the `DEF` dictionary. Package defaults' purpose
-# is different from that of package / thermodynamics constants (see constants.jl), which is
-# included after thermodynamic amounts are defined.
-
 """
-`const DEF = Dict{Symbol,Type{𝘁} where 𝘁<:AbstractThermodynamics}(...)`\n
-Package defaults: a dictionary of `{Symbol,Type{𝘁} where 𝘁<:AbstractThermodynamics}` pairs.\n
+`const DEF = Dict{Symbol,Type{𝘁} where 𝘁<:EngTherm}(...)`\n
+`EngThermBase` defaults: a dictionary of `{Symbol,Type{𝘁} where 𝘁<:EngTherm}` pairs.\n
 """
-const DEF = Dict{Symbol,Type{𝘁} where 𝘁<:AbstractThermodynamics}(
+const DEF = Dict{Symbol,Type{𝘁} where 𝘁<:EngTherm}(
     :IB     => MA,      # Default Intensive Base
     :EB     => SY,      # Default Extensive Base
     :XB     => EX,      # Default Exactness Base
