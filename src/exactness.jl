3c5be2678dbde95e691f6d6a5522528123ac4bec
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2019-11-07 16:08:38 -0300

    UPD shorter type aliases exp on src/exactness.jl

--- src/exactness.jl
+++ src/exactness.jl
@@ -1,18 +1,30 @@
 #----------------------------------------------------------------------------------------------#
+#                        Unexported Type Aliases -- Shorter Signatures                         #
+#----------------------------------------------------------------------------------------------#
+
+const F16 = Float16
+const F32 = Float32
+const F64 = Float64
+const BIG = BigFloat
+
+const MEA = Measurement
+const QTY = Quantity
+
+#----------------------------------------------------------------------------------------------#
 #                                        Type Exactness                                        #
 #----------------------------------------------------------------------------------------------#
 
 # \sansF<TAB>: plain Julia Floats (since Measurements.Measurement <: AbstractFloat)
-𝖥 = Union{Float16,Float32,Float64,BigFloat}
+𝖥 = Union{F16,F32,F64,BIG}
 
 # Exact types: all type params are \bsans#<TAB>
-ETY{𝘁} = Quantity{𝘁} where 𝘁<:𝖥
+EXT{𝘁} = QTY{𝘁} where 𝘁<:𝖥
 
 # Measurement types
-MTY{𝘁} = Quantity{Measurement{𝘁}} where 𝘁<:𝖥
+MMT{𝘁} = QTY{MEA{𝘁}} where 𝘁<:𝖥
 
 # θ Quantity types
-QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:𝖥
+AMT{𝘁} = Union{EXT{𝘁},MMT{𝘁}} where 𝘁<:𝖥
 
 
 #----------------------------------------------------------------------------------------------#
@@ -21,7 +33,7 @@ QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:𝖥
 
 # The 4 type quadrants are:
 # -------------------------
-#   bareF   QTY{𝘁}  | float-based
+#   bareF   AMT{𝘁}  | float-based
 #   bareR   unitR   | non-float reals
 #   -----------------
 #   plain   united
@@ -30,12 +42,12 @@ QTY{𝘁} = Union{ETY{𝘁},MTY{𝘁}} where 𝘁<:𝖥
 𝖱 = Union{AbstractIrrational,Integer,Rational}
 
 # Bare, unitless floats
-bareF = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖥
+bareF = Union{𝘁, MMT{𝘁}} where 𝘁<:𝖥
 
 # Bare, unitless reals
-bareR = Union{𝘁, Measurement{𝘁}} where 𝘁<:𝖱
+bareR = Union{𝘁, MMT{𝘁}} where 𝘁<:𝖱
 
 # United reals
-unitR = Union{Quantity{𝘁}, Quantity{Measurement{𝘁}}} where 𝘁<:𝖱
+unitR = Union{QTY{𝘁}, QTY{MMT{𝘁}}} where 𝘁<:𝖱
 
 
