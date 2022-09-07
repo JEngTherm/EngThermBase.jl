4613fde676bec4e71f8845ff92a60084c89eebc5
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2022-09-07 20:12:17 -0300

    UPD added abstract tests -- 4 of 4 tests passing

--- test/abstract.test.jl
+++ test/abstract.test.jl
@@ -1,8 +1,23 @@
+using InteractiveUtils
+
+function union2vec(theU::Union)
+    ret = DataType[]
+    while theU isa Union
+        push!(ret, theU.a)
+        theU = theU.b
+    end
+    push!(ret, theU)
+    return ret
+end
+
 #----------------------------------------------------------------------------------------------#
 #                                       abstract.test.jl                                       #
 #----------------------------------------------------------------------------------------------#
 
-@testset "abstract.test.jl:                                                       " begin
-    # @test ...
+@testset "abstract.test.jl: BASES branch                                          " begin
+    # All PREC members must be floats:
+    for __t in union2vec(EngThermBase.PREC)
+        @test __t <: AbstractFloat
+    end
 end
 
