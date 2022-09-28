2e7a72e0e7ca7d6b07b763ba424f5ac6fed95d87
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2022-09-28 14:15:11 -0300

    UPD the docs/make.jl after DrWatson's for themes

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,48 @@
+# Thanks to DrWatson's Documenter setup!
+cd(@__DIR__)
+using Pkg
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+CI && Pkg.activate(@__DIR__)
+CI && Pkg.instantiate()
+
 using Documenter
+using DocumenterTools: Themes
+
 using EngThermBase
 
-# Thanks to DrWatson's Documenter setup!
+# %%
+# download the themes
+for file in (
+        "juliadynamics-lightdefs.scss",
+        "juliadynamics-darkdefs.scss",
+        "juliadynamics-style.scss"
+    )
+    download(
+        "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file",
+        joinpath(@__DIR__, file)
+    )
+end
+
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
+end
+
+# compile the themes
+Themes.compile(
+    joinpath(@__DIR__, "juliadynamics-light.scss"),
+    joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(
+    joinpath(@__DIR__, "juliadynamics-dark.scss"),
+    joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
+# Translate .jl sources into .md ones
 using Literate
 Literate.markdown("src/tagging.jl", "src"; credit = false)
 
+# EngThermBase docs:
 makedocs(
     sitename = "EngThermBase",
     format = Documenter.HTML(
