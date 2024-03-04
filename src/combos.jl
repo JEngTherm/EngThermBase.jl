eb9a1a2c43b82c6cc16a5f12a6359cdec4b90368
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-03-04 08:51:17 -0300

    UPD the src/combos.jl

--- src/combos.jl
+++ src/combos.jl
@@ -3,6 +3,58 @@
 #==============================================================================================#
 
 #----------------------------------------------------------------------------------------------#
+#                                       ChF Pair Combos                                        #
+#----------------------------------------------------------------------------------------------#
+
+#······························································································#
+#                                         TvPair{𝕡,𝕩}                                          #
+#······························································································#
+
+struct TvPair{𝕡,𝕩,𝕓} <: ChFPair{𝕡,𝕩} where 𝕓
+    T::T_amt{𝕡,𝕩}
+    v::v_amt{𝕡,𝕩,𝕓}
+    # Inner, non-converting constructor
+    TvPair(_T::T_amt{𝕡,𝕩}, _v::v_amt{𝕡,𝕩,𝕓}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
+        @assert pod(_T) > zero(𝕡)
+        @assert pod(_v) > zero(𝕡)
+        new{𝕡,𝕩,𝕓}(_T, _v)
+    end
+    # Fallback constructors
+    TvPair(_v::v_amt{𝕡,𝕩,𝕓}, _T::T_amt{𝕡,𝕩}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
+        TvPair(_T, _v)
+    end
+    TvPair(_x::Tuple{T_amt{𝕡,𝕩}, v_amt{𝕡,𝕩,𝕓}}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
+        TvPair(_x...)
+    end
+    TvPair(_x::Tuple{v_amt{𝕡,𝕩,𝕓}, T_amt{𝕡,𝕩}}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
+        TvPair(_x[2], _x[1])
+    end
+    # Missing argument constructors
+    TvPair(_v::v_amt{𝕡,𝕩,𝕓}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = TvPair(T_(𝕡,𝕩), _v)
+end
+
+# External, converting constructors
+(::TvPair{𝕡,𝕩,𝕓})(x::TvPair{𝕤,𝕪,𝕓}) where {𝕡<:PREC,𝕤<:PREC,𝕩<:EXAC,𝕪<:EXAC,𝕓<:IntBase} = begin
+    TvPair(T_amt{𝕡,𝕩}(x.T), v_amt{𝕡,𝕩,𝕓}(x.v))
+end
+
+# Promotion rules
+promote_rule(::Type{TvPair{𝕤,𝕪}},
+             ::Type{TvPair{𝕡,𝕩}}) where {𝕤<:PREC,𝕡<:PREC,𝕪<:EXAC,𝕩<:EXAC} = begin
+    TvPair{promote_type(𝕤,𝕡),promote_type(𝕪,𝕩)}
+end
+
+# Conversions
+convert(::Type{TvPair{𝕤,𝕪}},
+        y::TvPair{𝕡,𝕩}) where {𝕤<:PREC,𝕡<:PREC,𝕪<:EXAC,𝕩<:EXAC} = begin
+    TvPair{promote_type(𝕤,𝕡),promote_type(𝕪,𝕩)}(y)
+end
+
+# Exporting
+export TvPair
+
+
+#----------------------------------------------------------------------------------------------#
 #                                       EoS Pair Combos                                        #
 #----------------------------------------------------------------------------------------------#
 
@@ -49,54 +101,6 @@ end
 
 # Exporting
 export TPPair
-
-
-#······························································································#
-#                                         TvPair{𝕡,𝕩}                                          #
-#······························································································#
-
-struct TvPair{𝕡,𝕩,𝕓} <: EoSPair{𝕡,𝕩} where 𝕓
-    T::T_amt{𝕡,𝕩}
-    v::v_amt{𝕡,𝕩,𝕓}
-    # Inner, non-converting constructor
-    TvPair(_T::T_amt{𝕡,𝕩}, _v::v_amt{𝕡,𝕩,𝕓}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
-        @assert pod(_T) > zero(𝕡)
-        @assert pod(_v) > zero(𝕡)
-        new{𝕡,𝕩,𝕓}(_T, _v)
-    end
-    # Fallback constructors
-    TvPair(_v::v_amt{𝕡,𝕩,𝕓}, _T::T_amt{𝕡,𝕩}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
-        TvPair(_T, _v)
-    end
-    TvPair(_x::Tuple{T_amt{𝕡,𝕩}, v_amt{𝕡,𝕩,𝕓}}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
-        TvPair(_x...)
-    end
-    TvPair(_x::Tuple{v_amt{𝕡,𝕩,𝕓}, T_amt{𝕡,𝕩}}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = begin
-        TvPair(_x[2], _x[1])
-    end
-    # Missing argument constructors
-    TvPair(_v::v_amt{𝕡,𝕩,𝕓}) where {𝕡<:PREC, 𝕩<:EXAC, 𝕓<:IntBase} = TvPair(T_(𝕡,𝕩), _v)
-end
-
-# External, converting constructors
-(::TvPair{𝕡,𝕩,𝕓})(x::TvPair{𝕤,𝕪,𝕓}) where {𝕡<:PREC,𝕤<:PREC,𝕩<:EXAC,𝕪<:EXAC,𝕓<:IntBase} = begin
-    TvPair(T_amt{𝕡,𝕩}(x.T), v_amt{𝕡,𝕩,𝕓}(x.v))
-end
-
-# Promotion rules
-promote_rule(::Type{TvPair{𝕤,𝕪}},
-             ::Type{TvPair{𝕡,𝕩}}) where {𝕤<:PREC,𝕡<:PREC,𝕪<:EXAC,𝕩<:EXAC} = begin
-    TvPair{promote_type(𝕤,𝕡),promote_type(𝕪,𝕩)}
-end
-
-# Conversions
-convert(::Type{TvPair{𝕤,𝕪}},
-        y::TvPair{𝕡,𝕩}) where {𝕤<:PREC,𝕡<:PREC,𝕪<:EXAC,𝕩<:EXAC} = begin
-    TvPair{promote_type(𝕤,𝕡),promote_type(𝕪,𝕩)}(y)
-end
-
-# Exporting
-export TvPair
 
 
 #······························································································#
