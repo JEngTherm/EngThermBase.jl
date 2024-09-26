534b53afac7a752e750eacb33f20d434930c5d28
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-09-26 19:18:07 -0300

    UPD added tests. All 285 tests passing

--- test/exactness.test.jl
+++ test/exactness.test.jl
@@ -7,7 +7,10 @@
     for 𝗣 in (Float16, Float32, Float64, BigFloat)
         @test _qty(𝗣(π)) isa Quantity{𝗣, NoDims}
         @test _qty(𝗣(π) ± 𝗣(π/100)) isa Quantity{Measurement{𝗣}, NoDims}
-        for 𝗨 in (u"m", u"s", u"N", u"J", u"K", u"mol", u"kJ/kg", u"kJ/kg/K")
+        for 𝗨 in (
+            u"m", u"s", u"m/s", u"N", u"N/m^2", u"J", u"J/s", u"K", u"1/K", u"mol",
+            u"mol/s", u"kg/kmol", u"kJ/kg", u"kJ/kg/s", u"kJ/kg/K", u"kJ/kg/K/s",
+        )
             @test _qty(𝗣(ℯ) * 𝗨) isa Quantity{𝗣, dimension(𝗨)}
             @test _qty((𝗣(ℯ) ± 𝗣(ℯ/100)) * 𝗨) isa Quantity{Measurement{𝗣}, dimension(𝗨)}
         end
