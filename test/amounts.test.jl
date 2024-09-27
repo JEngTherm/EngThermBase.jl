bf71fd5266fcca4f6d4554404f11d1d7f69c6d11
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-09-26 21:02:00 -0300

    ADD a new test/amounts.test.jl

---
+++ test/amounts.test.jl
@@ -0,0 +1,82 @@
+#----------------------------------------------------------------------------------------------#
+#                                       amounts.test.jl                                        #
+#----------------------------------------------------------------------------------------------#
+
+@testset "amounts.test.jl: Generic Amount tests                                   " begin
+    for 𝕋 in (:__amt, )
+        for ℙ in (Float16, Float32, Float64, BigFloat)
+            for 𝕌 in (
+                u"m", u"s", u"m/s", u"N", u"N/m^2", u"J", u"J/s", u"K", u"1/K", u"mol",
+                u"mol/s", u"kg/kmol", u"kJ/kg", u"kJ/kg/s", u"kJ/kg/K", u"kJ/kg/K/s",
+            )
+                #--------------------------------------------------------------------------#
+                #                            Exact Constructors                            #
+                #--------------------------------------------------------------------------#
+                amnt = eval(𝕋)(one(ℙ))                          # unitless arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,EX}
+                amnt = eval(𝕋)(one(ℙ) * 𝕌)                      # unit-ed arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,EX}
+                amnt = eval(𝕋)(amnt)                            # copy constructor
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,EX}
+                #--------------------------------------------------------------------------#
+                #                         Measurements Constructor                         #
+                #--------------------------------------------------------------------------#
+                amnt = eval(𝕋)(one(ℙ) ± one(ℙ)/ℙ(10))           # unitless arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,MM}
+                amnt = eval(𝕋)((one(ℙ) ± one(ℙ)/ℙ(10)) * 𝕌)     # unit-ed arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,MM}
+                amnt = eval(𝕋)(amnt)                            # copy constructor
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){ℙ}
+                @test amnt isa eval(𝕋){ℙ,MM}
+        end
+        for 𝕍 in REAL[Irrational{:ℯ}, Irrational{:π}, 2//3, 1//10, 3, 2]
+            for 𝕌 in (
+                u"m", u"s", u"m/s", u"N", u"N/m^2", u"J", u"J/s", u"K", u"1/K", u"mol",
+                u"mol/s", u"kg/kmol", u"kJ/kg", u"kJ/kg/s", u"kJ/kg/K", u"kJ/kg/K/s",
+            )
+                #--------------------------------------------------------------------------#
+                #                            Exact Constructors                            #
+                #--------------------------------------------------------------------------#
+                amnt = eval(𝕋)(𝕍)                               # unitless arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,EX}
+                amnt = eval(𝕋)(𝕍 * 𝕌)                           # unit-ed arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,EX}
+                amnt = eval(𝕋)(amnt)                            # copy constructor
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,EX}
+                #--------------------------------------------------------------------------#
+                #                         Measurements Constructor                         #
+                #--------------------------------------------------------------------------#
+                amnt = eval(𝕋)(𝕍 ± 𝕍)                           # unitless arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,MM}
+                amnt = eval(𝕋)((𝕍 ± 𝕍) * 𝕌)                     # unit-ed arg
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,MM}
+                amnt = eval(𝕋)(amnt)                            # copy constructor
+                @test amnt isa eval(𝕋)
+                @test amnt isa eval(𝕋){Float64}
+                @test amnt isa eval(𝕋){Float64,MM}
+        end
+    end
+end
+
+
